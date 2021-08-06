2109b547ddc437bd38cc77fb2ddb2774de436347
Jhonatas Menezes <jhoniedw.com@gmail.com>
2021-08-06 18:02:56 -0300

    p

---
+++ Django/tutorialdjango/blog/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255, unique=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    
+    
+    def __str__(self):
+        return self.title
