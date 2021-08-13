bb3d8b1d7faab5f171e998f1e3556a9833c95a68
Jhonatas Menezes <jhoniedw.com@gmail.com>
2021-08-13 20:28:21 -0300

    Commit de segurança

--- Django/TutorialDjango2/tutorial2/blog/admin.py
+++ Django/TutorialDjango2/tutorial2/blog/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Post
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title','slug','author','created','updated')
+    prepopulated_fields = {'slug':('title',)}
--- Django/TutorialDjango2/tutorial2/blog/views.py
+++ Django/TutorialDjango2/tutorial2/blog/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.views.generic import DetailView, ListView
+from .models import Post
 
-# Create your views here.
+class PostListView(ListView):
+    model = Post
+    
+class PostDetailView(DetailView):
+    model = Post
