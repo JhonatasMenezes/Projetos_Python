f412c1e44c4ab9145e2eb639ed73c9d99404fb98
Jhonatas Menezes <jhoniedw.com@gmail.com>
2021-08-13 22:10:15 -0300

    Adição de css e formatação de site.

--- Django/TutorialDjango2/tutorial2/blog/models.py
+++ Django/TutorialDjango2/tutorial2/blog/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import User
 from django.db import models
+from django.urls import reverse
 
 class Post(models.Model):
     title = models.CharField(max_length=255)
@@ -9,5 +10,13 @@ class Post(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     
+    class Meta:
+        ordering = ('-created',)
+    
+    
     def __str__(self):
         return self.title
+        
+    def get_absolute_url(self):
+        return reverse("blog:detail", kwargs={"slug": self.slug})
+    
