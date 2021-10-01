9452576852e3ee4c238744a69d53e1cde6c60bff
Jhonatas Menezes <jhoniedw.com@gmail.com>
2021-10-01 08:59:24 -0300

    Fazendo alterações no projeto e buscando melhorar o código.

--- Programas Simples/Calculadora com Cotações/main.py
+++ Programas Simples/Calculadora com Cotações/main.py
@@ -1,76 +1,31 @@
 import PySimpleGUI.PySimpleGUI as sg
-from modulos.moedas_cotacao import *
+import layout as lay
 import os
 import time
 
-# pegando cotações
-cotacoes = [str(dolar()),
-            str(euro()),
-            str(bitcoin())]
+layout = lay.layout
+menu_layout = lay.menu_layout
 
 # tema da janela
 sg.theme('Reddit')
-#layout do menu
-menu_layout = [['Arquivo',['Salvar', 'Sair']], 
-               ['Ajuda', ['Sobre']]]
-# layout da janela toda
-layout = [
-    # layout menu
-    [sg.Menu(menu_layout)],
-    # visor de entrada e respostas
-    [sg.Input(key='entrada',size=(25,3), background_color='gray',font=('Arial 22'))],
-    # cotações das moedas
-    [sg.Text(text='Cotação Dólar    R$',font=('Arial 12')),sg.Text(text=cotacoes[0],font=('Arial 12'))],
-    [sg.Text(text='Cotação Euro     R$',font=('Arial 12')),sg.Text(text=cotacoes[1],font=('Arial 12'))],
-    [sg.Text(text='Cotação Bitcoin  R$',font=('Arial 12')),sg.Text(text=cotacoes[2],font=('Arial 12'))],
-    # primeira linha de botões
-    [sg.Button('/',size=(3,1),font=('Arial 20'),key='dividir'), 
-        sg.Button('x',size=(3,1),font=('Arial 20'),key='multiplicar'), 
-        sg.Button('-',size=(3,1),font=('Arial 20'),key='subtrair'), 
-        sg.Button('+',size=(3,1),font=('Arial 20'),key='somar')],
-    # segunda linha de botões
-    [sg.Button('7',size=(3,1),font=('Arial 20'),key='sete'), 
-        sg.Button('8',size=(3,1),font=('Arial 20'),key='oito'), 
-        sg.Button('9',size=(3,1),font=('Arial 20'),key='nove'), 
-        sg.Button('%',size=(3,1),font=('Arial 20'),key='porcentagem')],
-    # terceira linha de botões
-    [sg.Button('4',size=(3,1),font=('Arial 20'),key='quatro'), 
-        sg.Button('5',size=(3,1),font=('Arial 20'),key='cinco'), 
-        sg.Button('6',size=(3,1),font=('Arial 20'),key='seis'), 
-        sg.Button('C',size=(3,1),font=('Arial 20'),key='clear')],
-    # quarta linha de botões
-    [sg.Button('1',size=(3,1),font=('Arial 20'),key='um'), 
-        sg.Button('2',size=(3,1),font=('Arial 20'),key='dois'), 
-        sg.Button('3',size=(3,1),font=('Arial 20'),key='tres'), 
-        sg.Button('<<',size=(3,1),font=('Arial 20'),key='backspace')],
-    # quinta linha de botões
-    [sg.Button('^',size=(3,1),font=('Arial 20'),key='potencia'), 
-        sg.Button('0',size=(3,1),font=('Arial 20'),key='zero'), 
-        sg.Button(',',size=(3,1),font=('Arial 20'),key='virgula'), 
-        sg.Button('=',size=(3,1),font=('Arial 20'),key='igual')]
-]
 
-class App():    
+class App:    
     def __init__(self) -> None:
-        self.janela = sg.Window('Calculadora Invest',
-                                layout, 
-                                size=(280,450),
-                                resizable=True,
-                                return_keyboard_events=False,
-                                finalize=True)
+        self.janela = sg.Window('Calculadora Invest', layout, size=(280,450), return_keyboard_events=False)
         self.result = 0
         self.janela.read(timeout=5)
-        self.numEvento = {'zero':0, 'um':1, 'dois':2, 'tres':3, 'quatro':4, 'cinco':5, 'seis':6, 'sete':7, 'oito':8, 'nove':9,'virgula':','}
+        self.numEvento = {'zero':0, 'um':1, 'dois':2, 'tres':3, 'quatro':4, 'cinco':5,
+                          'seis':6, 'sete':7, 'oito':8, 'nove':9,'virgula':','}
         self.operEvento = {'somar':'+', 'subtrair':'-', 'multiplicar':'x', 'dividir':'/','porcentagem':'%','potencia':'^'}
         
-    # for i in range(0, 4):
-    #     for button in layout[i]:
-    #         button.expand(expand_x=True,expand_y=True)
+    
+    def sair(self):
+        os._exit()
+    
     
     def about(self):
-        sg.popup('Sobre', 
-                 'Para mais informações, acesse o link:',
-                 'github.com/JhonatasMenezes')
+        sg.popup('Sobre','Para mais informações, acesse o link:','github.com/JhonatasMenezes')
+    
     
     def resultado(self,oper,val1,val2):
         if oper == '+':
@@ -128,7 +83,3 @@ class App():
     
 calculadora = App()
 calculadora.start()         
-            
-            
-            
-            
